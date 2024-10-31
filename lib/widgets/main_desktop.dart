3937015137ab108062b60f7210da2ab161913f0e
thrilokprakashh <thrilokprakashan@gmail.com>
2024-10-31 15:42:30 +0530

    updates

--- lib/widgets/main_desktop.dart
+++ lib/widgets/main_desktop.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:thrilok_portfolio/constants/colors.dart';
+import 'dart:js' as js;
 
-class MainDesktop extends StatelessWidget {
+class MainDesktop extends StatefulWidget {
   const MainDesktop({super.key});
 
   @override
+  State<MainDesktop> createState() => _MainDesktopState();
+}
+
+class _MainDesktopState extends State<MainDesktop>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _scaleAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    // Scale animation from 0.5 to 1.0
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    // Start the animation
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
     final screenHeight = screenSize.height;
     return Container(
-      margin:const EdgeInsets.symmetric(horizontal: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 20),
       height: screenHeight / 1.2,
       constraints: const BoxConstraints(minHeight: 350),
       child: Row(
@@ -20,28 +52,42 @@ class MainDesktop extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-             const Text(
-                "Hi,\nI'm Thrilok Prakash\nA Flutter Developer",
-                style: TextStyle(
-                    color: CustomColor.whitePrimary,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    height: 1.5),
+              FadeTransition(
+                opacity: _opacityAnimation,
+                child: const Text(
+                  "Hi,\nI'm Thrilok Prakash\nA Flutter Developer",
+                  style: TextStyle(
+                      color: CustomColor.whitePrimary,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      height: 1.5),
+                ),
               ),
-            const  SizedBox(height: 20),
+              const SizedBox(height: 20),
               SizedBox(
                 width: 250,
                 height: 50,
                 child: ElevatedButton(
-                  onPressed: () {},
-                  child:const Text("Get in touch"),
+                  onPressed: () {
+                    js.context.callMethod(
+                      "open",
+                      ["https://github.com/thrilokprakashh/facebook_cloneApp"],
+                    );
+                  },
+                  child: const Text(
+                    "Download CV",
+                    style: TextStyle(color: CustomColor.whitePrimary),
+                  ),
                 ),
               ),
             ],
           ),
-          Image.asset(
-            "assets/my_flutter_avatar.png",
-            width: screenWidth / 2,
+          ScaleTransition(
+            scale: _scaleAnimation,
+            child: Image.asset(
+              "assets/my_flutter_avatar.png",
+              width: screenWidth / 2,
+            ),
           ),
         ],
       ),
--- lib/widgets/main_mobile.dart
+++ lib/widgets/main_mobile.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:thrilok_portfolio/constants/colors.dart';
+import 'dart:js' as js;
 
-class MainMobile extends StatelessWidget {
+class MainMobile extends StatefulWidget {
   const MainMobile({super.key});
 
   @override
+  State<MainMobile> createState() => _MainMobileState();
+}
+
+class _MainMobileState extends State<MainMobile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<double> _scaleAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
+
+    _opacityAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    // Scale animation from 0.5 to 1.0
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    // Start the animation
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     final screenHeight = screenSize.height;
@@ -29,29 +61,42 @@ class MainMobile extends StatelessWidget {
               ).createShader(bounds);
             },
             blendMode: BlendMode.srcATop,
-            child: Image.asset(
-              "assets/my_flutter_avatar.png",
-              width: screenWidth,
+            child: ScaleTransition(
+              scale: _scaleAnimation,
+              child: Image.asset(
+                "assets/my_flutter_avatar.png",
+                width: screenWidth,
+              ),
             ),
           ),
           const SizedBox(
             height: 30,
           ),
-          const Text(
-            "Hi,\nI'm Thrilok Prakash\nA Flutter Developer",
-            style: TextStyle(
-                color: CustomColor.whitePrimary,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                height: 1.5),
+          FadeTransition(
+            opacity: _opacityAnimation,
+            child: Text(
+              "Hi,\nI'm Thrilok Prakash\nA Flutter Developer",
+              style: TextStyle(
+                  color: CustomColor.whitePrimary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  height: 1.5),
+            ),
           ),
           const SizedBox(height: 20),
           SizedBox(
             width: 180,
             height: 50,
             child: ElevatedButton(
-              onPressed: () {},
-              child: const Text("Get in touch"),
+              onPressed: () {
+                js.context.callMethod("open", [
+                  "https://drive.google.com/file/d/1f0vMNVfu5odIJY73KjRQMmo-y4TVbpE-/view?usp=share_link"
+                ]);
+              },
+              child: const Text(
+                "Download CV",
+                style: TextStyle(color: CustomColor.whitePrimary),
+              ),
             ),
           ),
         ],
